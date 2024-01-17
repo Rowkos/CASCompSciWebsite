b7cbc132236d2a550a0e5dd1af26b75b9a63d1ac
Rowkos <rowankoskoff@gmail.com>
2024-01-17 10:25:35 -0800

    started confirmation page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,19 +7,30 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+
 @app.route("/about")
 def about():
     return render_template("about.html")
+
 
 @app.route("/products")
 def products():
     return render_template("products.html")
 
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
 
+@app.route("/confirmation")
+def confirmation():
+    email = request.args.get("email")
+    address = request.args.get("address")
+    print(email, address)
+
+    props = {"address": address, "email": email}
+    return render_template("confirmation.html", data=props)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-# todo, add a link to the home page in the nav
